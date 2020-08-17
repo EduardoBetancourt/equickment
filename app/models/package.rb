b8fc9a44b5c649586727e9276d9b52e34a7a639a
Eduardo Betancourt <ebhdz@hotmail.com>
2020-08-17 17:14:27 -0500

    models

---
+++ app/models/package.rb
@@ -0,0 +1,8 @@
+class Package < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+end
