ba61522b64ba6d1f951f97e272b1e99cea649c54
Eduardo Betancourt <ebhdz@hotmail.com>
2020-08-18 16:23:53 -0500

    We can now book directly from a package

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bookings, only: :index
   resources :packages do
-    resources :bookings, only: %i[new create]
+    resources :bookings, only: :create
   end
 end
