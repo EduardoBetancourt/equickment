b844aa4e4d3675333e4c49c9af708f34bf029b4a
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-08-18 13:07:15 -0500

    bookings controller and index view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,28 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def new
+    @package = Package.find(params[:package_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @package = Package.find(params[:package_id])
+    @booking.user = current_user
+    @booking.package = @package
+    if @booking.save
+      redirect_to package_path(@package)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
