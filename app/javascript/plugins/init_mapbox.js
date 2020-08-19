3014fc12d1f4f06d52d2418bc2c1129f30f4ac53
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-08-19 18:00:38 -0500

    some front end fixes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,6 +12,7 @@ const buildMap = () => {
 };
 
 const addMarkersToMap = (map, markers) => {
+  console.log(markers)
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     new mapboxgl.Marker()
@@ -24,7 +25,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 5, maxZoom: 13 });
 };
 
 const initMapbox = () => {
