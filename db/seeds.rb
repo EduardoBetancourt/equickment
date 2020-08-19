88268e81365f0dc57b52ae4e280a48e87117fa5d
Suzan van Borkum <suzan.vanborkum@gmail.com>
2020-08-19 12:13:08 -0500

    addresses added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,13 @@ require 'faker'
   User.create!(email: Faker::Internet.email, password: "password")
 end
 
-Package.create!(name: "Movie Night" , description: "We'll bring popcorn, a projector and will get you wet in splashy scenes", price: rand(0..1000), user: User.all.sample)
-Package.create!(name: "Barbecue Evening" , description: "No grill, no problem. We'll even set up the charcoal for you", price: rand(0..1000), user: User.all.sample)
-Package.create!(name: "Dinner for two" , description: "Have a romantic dinner for that special someone without setting a foot outside", price: rand(0..1000), user: User.all.sample)
-Package.create!(name: "Nintendo Switch" , description: "Are you still good at Mario Kart? Enjoy the nintendo wihtout having to buy one", price: rand(0..1000), user: User.all.sample)
-Package.create!(name: "Take My Puppy For a Walk" , description: "Are you alone this quarantine and want some company? Don't worry, you can take my puppy for a walk", price: rand(0..1000), user: User.all.sample)
-Package.create!(name: "LawnMower" , description: "Why are gardners alawys happy? Becuase they mow land, don't skip this opportunity for you to experience true happiness", price: rand(0..1000), user: User.all.sample)
-Package.create!(name: "Bad Bunny for a Night" , description: "I'll dress as BadBunny, setup a professional concert and sing to you", price: rand(0..1000), user: User.all.sample)
-
+Package.create!(name: "Movie Night", address: "Simón Bolívar 320, Ciudad de México, CDMX", description: "We'll bring popcorn, a projector and will get you wet in splashy scenes", price: rand(0..1000), user: User.all.sample)
+Package.create!(name: "Barbecue Evening", address: "Calle de Venustiano Carranza 49, Ciudad de México, CDMX", description: "No grill, no problem. We'll even set up the charcoal for you", price: rand(0..1000), user: User.all.sample)
+Package.create!(name: "Dinner for two", address: "Hidalgo 184, Centro, Santiago de Querétaro, Qro.", description: "Have a romantic dinner for that special someone without setting a foot outside", price: rand(0..1000), user: User.all.sample)
+Package.create!(name: "Nintendo Switch", address: "Calle Ezequiel Montes 55, Santiago de Querétaro, Qro.", description: "Are you still good at Mario Kart? Enjoy the nintendo wihtout having to buy one", price: rand(0..1000), user: User.all.sample)
+Package.create!(name: "Take My Puppy For a Walk", address: "Calle 51 Este, Panamá, Panama", description: "Are you alone this quarantine and want some company? Don't worry, you can take my puppy for a walk", price: rand(0..1000), user: User.all.sample)
+Package.create!(name: "LawnMower", address: "Calle 72 Este, Panamá, Panama", description: "Why are gardners alawys happy? Becuase they mow land, don't skip this opportunity for you to experience true happiness", price: rand(0..1000), user: User.all.sample)
+Package.create!(name: "Bad Bunny for a Night", address: "Tokio 824, Portales Sur, Benito Juárez, 03300 Ciudad de México, CDMX", description: "I'll dress as BadBunny, setup a professional concert and sing to you", price: rand(0..1000), user: User.all.sample)
 
 
 
